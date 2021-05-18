2ac885b80b1db8db070557a9521bee039c57d2cf
ajay kumar <asalwania@gmail.com>
2021-05-18 12:23:47 +0530

    day-2 typescript type

--- day-2/index.ts
+++ day-2/index.ts
@@ -14,6 +14,10 @@ const user1: {
   age: 29,
 };
 
+let now: Date = new Date();
+
+const newArray: string[] = ["Ajay", "Pooja", "Alka", "Kamlesh"];
+
 // we can also use interface for object shape
 interface User {
   name: string;
@@ -27,29 +31,69 @@ const user2: User = {
 
 // we can use interface with function
 
-function getUser():User{
-    return {
-        name:'Vinod',
-        age:34
-    }
+function getUser(): User {
+  return {
+    name: "Vinod",
+    age: 34,
+  };
 }
 
 // we can also use an interface with classes
-class UserAccount{
-    name:string;
-    age:number
+class UserAccount {
+  name: string;
+  age: number;
 
-    constructor(name:string,age:number){
-        this.name=name;
-        this.age = age;
-    }
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
 }
 
-const user3:User = new UserAccount('Amit',31)
-
+const user3: User = new UserAccount("Amit", 31);
 
 // Union
-let isNumber:number | boolean = 1 // in union we can use two or more than two data types
-isNumber = true
+let isNumber: number | boolean = 1; // in union we can use two or more than two data types
+isNumber = true;
 
+// object destructing
 
+const todaysWeather = {
+  date: new Date(),
+  weather: "sunny",
+};
+
+const logWeather = (forcast: { date: Date; weather: string }) => {
+  console.log(forcast.date);
+  console.log(forcast.weather);
+};
+
+logWeather(todaysWeather);
+
+// annotations around objects
+const profile = {
+  name: "Ajay",
+  myAge: 29,
+  cord: {
+    lat: 0,
+    long: 15,
+  },
+  setAge(newAge: number): void {
+    this.age = newAge;
+  },
+};
+
+const { myAge }: { myAge: number } = profile;
+
+const {
+  cord: { lat, long },
+}: { cord: { lat: number; long: number } } = profile;
+
+// tuple
+
+const drink = {
+  color:'brown',
+  carbonated:true,
+  sugar:40
+}
+
+const pepsi:[string,boolean,number] =['brown',true,40]
